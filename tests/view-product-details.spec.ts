033e6969906fdb836a3b8b622ff243691c5ddd57
lidiia.novoseletska <lnykoyn@quicklert.com>
2026-09-05 18:48:25 +0300

    added authentication

--- tests/view-product-details.spec.ts
+++ tests/view-product-details.spec.ts
@@ -1,21 +1,16 @@
 import { test, expect } from '@playwright/test';
-import { LoginPage } from '../pages/login.page';
 import { AccountPage } from "../pages/account.page"
 import { HomePage } from '../pages/home.page';
 
-test.skip(!!process.env.CI, 'Login is blocked in GitHub CI');
+test.use({
+  storageState: 'playwright/.auth/user.json',
+});
 
-test.beforeEach(async ({ page }) => {
-    const loginPage = new LoginPage(page);
-    await page.goto('/auth/login');
-    await loginPage.performLogin('customer@practicesoftwaretesting.com', 'welcome01');
-    await expect(page).toHaveURL('/account');
-  });
 
 test('Verify user can view product details', async ({ page }) => {
     const accountPage = new AccountPage(page);
     const homePage = new HomePage(page);
-
+    await page.goto('/account');
     await accountPage.clickHomeBtn();
     await homePage.selectProductByName('Combination Pliers');
     await expect(page).toHaveURL(/\/product\//);
@@ -23,6 +18,5 @@ test('Verify user can view product details', async ({ page }) => {
     await expect(homePage.productPrice).toHaveText('14.15');
     await expect(homePage.addToCartBtn).toBeVisible();
     await expect(homePage.addToFavoriteBtn).toBeVisible();
-
 }
 )
