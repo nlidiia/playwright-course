24cb249dec66f85c3c68be66ad24edd0acb32ec8
lidiia.novoseletska <lnykoyn@quicklert.com>
2026-09-14 14:40:37 +0300

    fix view product details

--- tests/view-product-details.spec.ts
+++ tests/view-product-details.spec.ts
@@ -1,16 +1,12 @@
 import { test, expect } from '../fixtures';
 
 test('Verify user can view product details', async ({ loggedInApp }) => {
-    await loggedInApp.page.goto('/account');
-    await loggedInApp.accountPage.clickHomeBtn();
+    await loggedInApp.page.goto('/');
     await loggedInApp.homePage.selectProductByName('Combination Pliers');
     await expect(loggedInApp.page).toHaveURL(/\/product\//);
     await expect(loggedInApp.homePage.product).toHaveText('Combination Pliers');
     await expect(loggedInApp.homePage.productPrice).toHaveText('14.15');
     await expect(loggedInApp.homePage.addToCartBtn).toBeVisible();
     await expect(loggedInApp.homePage.addToFavoriteBtn).toBeVisible();
-    await loggedInApp.homePage.addToCartBtn.click();
-    await loggedInApp.homePage.cartShopping.click();
-    await expect(loggedInApp.page).toHaveURL('/checkout');
 }
 )
